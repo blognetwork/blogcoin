ffa927f950fa61993b77f87aed1a2816dfba94a1
TheDevMinerTV <tobigames200@gmail.com>
2019-06-16 10:05:59 +0200

    Add def as seed node

--- src/config/CryptoNoteConfig.h
+++ src/config/CryptoNoteConfig.h
@@ -263,6 +263,7 @@ const static   boost::uuids::uuid CRYPTONOTE_NETWORK         =
 };
 
 const char* const SEED_NODES[] = {
-    "5.172.219.174:54212"
+    "5.172.219.174:54212", // sniperviperman
+    "99.248.93.15:54212"   // def
 };
 } // CryptoNote
